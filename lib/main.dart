531c2c5736fb373c85acf6d67a8e9df026031e64
rizkinurlahid <greenrizky354@gmail.com>
2020-05-06 12:06:21 +0700

    Add uses-permission INTERNET

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:app_news/utils/color.dart';
 import 'package:app_news/views/pages/login.dart';
 import 'package:app_news/views/pages/mainMenu.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
